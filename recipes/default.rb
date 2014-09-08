60eea876c541a6514a60c423a5cfa61e88efa61e
Nils Landt <nils@promisedlandt.de>
2014-09-08 16:03:03 +0200

    Fix build-essential deprecation warning

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,12 +1,12 @@
 include_recipe "apt" if platform_family?("debian")
 
-node.set["build_essential"]["compiletime"] = true
+node.set["build_essential"]["compile_time"] = true
 include_recipe "build-essential"
 
 # TODO remove when build-essential debian bug is fixed (make not being installed)
-package "make" do
-  action :nothing
-end.run_action(:install)
+#package "make" do
+  #action :nothing
+#end.run_action(:install)
 
 chef_gem "deep_merge" do
   action :nothing
